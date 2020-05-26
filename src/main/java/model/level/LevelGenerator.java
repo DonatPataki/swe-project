dd07c294372bcbd6d353230d12e12add060d81c2
Pataki Donát <xpdonat@gmail.com>
2020-05-26 20:57:03 +0200

    fixed level generation

--- src/main/java/model/level/LevelGenerator.java
+++ src/main/java/model/level/LevelGenerator.java
@@ -11,11 +11,11 @@ public class LevelGenerator {
 
     private Random random = new Random();
     private Level level;
-    private final int ROOM_MAX_SIZE = 6;
-    private final int ROOM_MIN_SIZE = 4;
-    private final int MAX_ROOMS = 20;
 
     public Level generateLevel(int levelWidth, int levelHeight) {
+        final int ROOM_MAX_SIZE = 8;
+        final int ROOM_MIN_SIZE = 4;
+        final int MAX_ROOMS = 10;
 
         level = new Level(levelWidth, levelHeight);
         int numRooms = 0;
@@ -24,14 +24,14 @@ public class LevelGenerator {
         for (int i = 0; i < MAX_ROOMS + 1; i++) {
             int w = random.nextInt((ROOM_MAX_SIZE - ROOM_MIN_SIZE) + 1) + ROOM_MIN_SIZE;
             int h = random.nextInt((ROOM_MAX_SIZE - ROOM_MIN_SIZE) + 1) + ROOM_MIN_SIZE;
-            int x = random.nextInt(20 - w);
-            int y = random.nextInt(20 - h);
+            int x = random.nextInt(levelWidth - w);
+            int y = random.nextInt(levelHeight - h);
 
             Rectangle newRoom = new Rectangle(x, y, w, h);
 
             boolean overlapping = false;
-            for (int j = 0; j < rooms.size(); j++) {
-                if (newRoom.intersect(rooms.get(j))) {
+            for (Rectangle room : rooms) {
+                if (newRoom.intersect(room)) {
                     overlapping = true;
                     break;
                 }
@@ -46,10 +46,10 @@ public class LevelGenerator {
 
                     if (random.nextBoolean()) {
                         createHorTunnel(prevCenter, newCenter);
-                        createVerTunnel(prevCenter, newCenter);
+                        createVerTunnel(newCenter, prevCenter);
                     } else {
                         createVerTunnel(prevCenter, newCenter);
-                        createHorTunnel(prevCenter, newCenter);
+                        createHorTunnel(newCenter, prevCenter);
                     }
                 }
 
@@ -69,15 +69,15 @@ public class LevelGenerator {
         }
     }
 
-    private void createHorTunnel(Point previous, Point current) {
-        for (int x = Math.min(previous.getX(), current.getX()); x < Math.max(previous.getX(), current.getX()) + 1; x++) {
-            level.setValueAtLocation(x, previous.getY(), 1);
+    private void createHorTunnel(Point from, Point toward) {
+        for (int x = Math.min(from.getX(), toward.getX()); x < Math.max(from.getX(), toward.getX()) + 1; x++) {
+            level.setValueAtLocation(x, from.getY(), 1);
         }
     }
 
-    private void createVerTunnel(Point previous, Point current) {
-        for (int y = Math.min(previous.getY(), current.getY()); y < Math.max(previous.getY(), current.getY()) + 1; y++) {
-            level.setValueAtLocation(previous.getX(), y, 1);
+    private void createVerTunnel(Point from, Point toward) {
+        for (int y = Math.min(from.getY(), toward.getY()); y < Math.max(from.getY(), toward.getY()) + 1; y++) {
+            level.setValueAtLocation(from.getX(), y, 1);
         }
     }
 }
